7417669c15c0f67fb82554bace34285e0e2de22c
Carlos Dias <113734613+EpicuroTech@users.noreply.github.com>
2023-09-23 16:14:27 +0100

    strings

---
+++ Strings/Strings/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Strings
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exec = new System.Windows.Forms.Button();
+            this.cx_txt = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // exec
+            // 
+            this.exec.Location = new System.Drawing.Point(656, 350);
+            this.exec.Name = "exec";
+            this.exec.Size = new System.Drawing.Size(111, 62);
+            this.exec.TabIndex = 0;
+            this.exec.Text = "EXECUTAR";
+            this.exec.UseVisualStyleBackColor = true;
+            this.exec.Click += new System.EventHandler(this.exec_Click);
+            // 
+            // cx_txt
+            // 
+            this.cx_txt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cx_txt.Location = new System.Drawing.Point(12, 18);
+            this.cx_txt.Name = "cx_txt";
+            this.cx_txt.Size = new System.Drawing.Size(767, 166);
+            this.cx_txt.TabIndex = 1;
+            this.cx_txt.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cx_txt);
+            this.Controls.Add(this.exec);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exec;
+        private System.Windows.Forms.Label cx_txt;
+    }
+}
+
---
+++ Strings/stringcode/exec_label.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Strings
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exec_Click(object sender, EventArgs e)
+        {
+            //STRINGS
+
+            //string frase = "Variavel de texto alfanumerico    ";
+            //CONTAGEM DE LETRAS
+            //int nletras = frase.Length;
+            //cx_txt.Text = frase + nletras;
+            //cx_txt.Text = frase + " (contem " + frase.Length + " Letras!)";
+
+            //cx_txt.Text = frase.ToUpper();
+
+            //cx_txt.Text = frase.Substring(12, 5);//apartir de onde, e quantas letras)
+
+            //bool res = frase.Contains("alfa");
+            //cx_txt.Text = res.ToString();
+
+            //bool res = frase.StartsWith("Vari");
+            //cx_txt.Text = res.ToString();
+
+            //cx_txt.Text = frase.Trim(); //retira espaço do inicio e do fim de uma string
+
+            //Indice de caracter
+            //int x = frase.IndexOf("o");
+            //int z = frase.LastIndexOf("o");
+            //cx_txt.Text = x.ToString() + " e " + z.ToString();
+
+            //JUNTAR, neste teste adicionei apelino no meio dos 2 nomes, adicionando espaço
+            //antes do apelido, daí o 2º .insert
+            string nome = "Carlos Dias";
+            string apelido = "Vicente Salgueiro";
+            string[] pronome = nome.Split(' ');
+            string nome_completo = nome.Insert(pronome[0].Length, apelido.Insert(0, " "));
+            cx_txt.Text = nome_completo;
+
+            //TROCAR LETRA
+            //string nome = "Carlos Dias";
+            //string final = nome.Replace('i', '_');
+            //cx_txt.Text = final;
+
+            //SEPARAR STRING
+            //string items = "Carlos, João, Paulo, Manuel, Jorge";
+            //string[] nomes = items.Split(',');
+            //cx_txt.Text = nomes[0];
+        }
+    }
+}
